9d136aac37c5de8433556affe71644eac3c5b2ea
joao santana <joaosantana.ti@gmail.com>
2024-09-21 20:52:49 -0300

    fix: sample shows values when is a post request

--- Sample.php
+++ Sample.php
@@ -7,7 +7,7 @@ $form = new Form();
 $form->addInput(new TextInput("firstname", "First Name", "Bruce"));
 $form->addInput(new TextInput("lastname", "Last Name", "Wayne"));
 
-if ($_SERVER['METHOD']=="POST") {
+if ($_SERVER['REQUEST_METHOD']=="POST") {
     if ($form->validate()) {
         // display user info
         $firstName = $form->getValue("firstname");
